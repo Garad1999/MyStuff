4405079f5d61cd31a324941330ec34dd0c293e88
task2 week 1 <g18w8475@HUG124>
2020-02-20 15:10:40 +0200

    NUnit Test added to the repository.

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			Console.WriteLine("Input message:   ");
+			string userinput = Console.ReadLine();
+			string x = Prefix(userinput);
+			Console.WriteLine(x);
+			Console.ReadLine();
         }
+		static string Prefix (string input){
+			int counter = 0;
+			if (input == null) return null;
+			
+			for (int i = 0; i < input.Length; i++){
+				if (input[i] == ' ' || i == (input.Length -1)){
+					counter++;
+				}
+			}
+			
+			
+			/*StringBuilder str = new StringBuilder();
+			str.append(str(input.Length));
+			str.append(",");
+			str.append(str(counter));
+			str.append(":");
+			str.append(input); */
+			
+			//return str.toString();
+			return ("" + input.Length + "," + counter + ":" + input);
+			
+		}
+		
     }
 }
